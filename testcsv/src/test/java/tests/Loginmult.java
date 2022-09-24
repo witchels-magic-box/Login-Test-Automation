9f4bbdf1dd50d7cee07af9593a73809b1ebd43f4
madhu <madhurjodas@inneed.cloud>
2022-09-24 14:07:42 +0600

    Feat: Login Test Automation

---
+++ testcsv/src/test/java/tests/Loginmult.java
@@ -0,0 +1,134 @@
+package tests;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+
+import drivers.BaseDriver;
+import drivers.PageDriver;
+import pages.HomePage;
+
+public class Loginmult extends BaseDriver {
+
+	@Test
+	public void Login() throws IOException, InterruptedException, CsvValidationException
+
+	{
+		
+		String CSV_PATH = "C:\\Users\\madhu\\eclipse-workspace\\testcsv\\TestData\\csvfiling.csv";
+		String outputFile = "C:\\Users\\madhu\\eclipse-workspace\\testcsv\\TestData\\data.csv";
+
+		CSVReader csvReader;
+		String[] csvCell;
+		csvReader = new CSVReader(new FileReader(CSV_PATH));
+		CSVWriter csvOutput = new CSVWriter(new FileWriter(outputFile, true));
+		boolean CheckTrue = true;
+		String Result = null;
+		
+
+		HomePage homePage = new HomePage();
+		List<String[]> data = new ArrayList<String[]>();
+
+//			int Cellno = row.getLastCellNum();
+//			System.out.println("The Total column no's are : " + Cellno);
+
+		while ((csvCell = csvReader.readNext()) != null) {
+			
+            String Username1 = csvCell[0];
+            String Pass = csvCell[1];
+//            String Result = csvCell[2];
+			homePage.clickOnSignIn();
+			homePage.sendEmailLog(Username1);
+			homePage.SendPass(Pass);
+			homePage.SubmitClick();
+			Thread.sleep(5000);
+			try {
+				homePage.LogOut();
+				
+//				boolean Signout = homePage.CheckLogout();
+//				Assert.assertEquals(Signout, CheckTrue);
+
+				System.out.println(Username1 + " and Password: " + Pass + " is Passed");
+				Result = "Pass";
+//				Result = "PASS";
+				
+
+
+//				cell = row.createCell(2);
+//				cell.setCellValue(Result);
+
+			} catch (Exception e) {
+//					boolean Msg = homePage.Errordisplay();
+//					boolean CheckFalse = true;
+//					Assert.assertEquals(Msg, CheckFalse)
+
+				Result = "Fail";
+				System.out.println(Username1 + " and Password: " + Pass + " is Failed");
+//				Result = "FAIL";
+				homePage.clickOnSignIn();
+//				cell = row.createCell(2);
+//				cell.setCellValue(Result);
+//				homePage.clickOnSignIn();
+			}		
+				
+				data.add(new String[] {Username1,Pass,Result});
+				csvOutput.writeAll(data);
+				csvOutput.close();
+
+			}
+
+//			homePage.clickOnSignIn();
+//			homePage.sendEmailLog(Username);
+//			homePage.SendPass(Password);
+//			homePage.SubmitClick();
+//			Thread.sleep(5000);
+//
+//			try {
+//				boolean Signout = homePage.CheckLogout();
+//				Assert.assertEquals(Signout, CheckTrue);
+//				System.out.println(Username + " and Password: " + Password + " is Passed");
+//				Result = "PASS";
+//				homePage.LogOut();
+//				cell = row.createCell(2);
+//				cell.setCellValue(Result);
+//
+//			} catch (Exception e) {
+////					boolean Msg = homePage.Errordisplay();
+////					boolean CheckFalse = true;
+////					Assert.assertEquals(Msg, CheckFalse);
+//				System.out.println(Username + " and Password: " + Password + " is Failed");
+//				Result = "FAIL";
+//				cell = row.createCell(2);
+//				cell.setCellValue(Result);
+//				homePage.clickOnSignIn();
+//			}
+
+//		}
+//		FileOutputStream fos = new FileOutputStream(file);
+//		wb.write(fos);
+//		fos.close();
+
+	}
+
+	@BeforeClass
+	public void startUrl() {
+		PageDriver.getCurrentDriver().get(baseURL);
+		PageDriver.getCurrentDriver().manage().window().maximize();
+	}
+
+	//
+//			String username = cell.getStringCellValue();
+//			System.out.println("UserName is " + username);
+
+}
